c4fcaa539ce461d7ce311f22834aacdeda035980
Nesat Nayem <nesatnayem042@gmail.com>
2022-08-31 12:19:09 +0600

    done

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+// done 
 const Connection = async () => {
   const URL = `mongodb://nayemnesat:Lion2324@ac-v6hdw2i-shard-00-00.wqpn5pd.mongodb.net:27017,ac-v6hdw2i-shard-00-01.wqpn5pd.mongodb.net:27017,ac-v6hdw2i-shard-00-02.wqpn5pd.mongodb.net:27017/?ssl=true&replicaSet=atlas-3w4cbo-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
